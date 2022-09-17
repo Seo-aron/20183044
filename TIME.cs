0f0a67753d60ff616093f1dad5e6644900e866da
Seo-aron <113684586+Seo-aron@users.noreply.github.com>
2022-09-17 20:43:26 +0900

    Add files via upload

---
+++ TIME.cs
@@ -0,0 +1,22 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace _20183044
+//{
+//    internal class TIME
+//    {
+//        public static void Main()
+//        {
+//            int time, hour, minute, second;
+//            Console.WriteLine("***Enter an integral time: ");
+//            time = int.Parse(Console.ReadLine());
+//            hour = time / 10000;
+//            minute = time / 100 % 100;
+//            second = time % 100;
+//            Console.WriteLine("***Time is " + hour + ":" + minute + ":" + second);
+//        }
+//    }
+//}
