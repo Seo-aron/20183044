0ec8eb6d99a98a4f2cfac9701199c4dbeb8098d5
Seo-aron <113684586+Seo-aron@users.noreply.github.com>
2022-09-17 20:43:39 +0900

    Add files via upload

---
+++ KeySerch.cs
@@ -0,0 +1,20 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace _20183044
+//{
+//    internal class KeySerch
+//    {
+//        static void Main(string[] args)
+//        {
+//            while (true)
+//            {
+//                ConsoleKeyInfo name = Console.ReadKey();
+//                Console.WriteLine("\n You Press : {0}", name.Key);
+//            }
+//        }
+//    }
+//}
