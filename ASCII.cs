97e6dfeafcc9b5982d391da01b7d0c966fe7c3a5
Seo-aron <113684586+Seo-aron@users.noreply.github.com>
2022-09-17 20:42:54 +0900

    Add files via upload

---
+++ ASCII.cs
@@ -0,0 +1,17 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace _20183044
+//{
+//    internal class ASCII
+//    {
+//        static void Main(string[] args)
+//        {
+//            int value = Console.Read();
+//            Console.WriteLine("Read Result : {0}", value);
+//        }
+//    }
+//}
