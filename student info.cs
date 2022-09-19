ccee230565031b347601ce5281ce19c6e490c948
Seo-aron <113684586+Seo-aron@users.noreply.github.com>
2022-09-20 00:18:56 +0900

    Add files via upload

---
+++ student info.cs
@@ -0,0 +1,42 @@
+﻿//using System;
+
+//namespace _20183044{
+//    class student
+//    {
+//        private int rollno;
+//        private string name;
+//        private int age;
+
+//        public void Setinfo(string name, int rollno, int age)
+//        {
+//            this.rollno = rollno;
+//            this.age = age;
+//            this.name = name;
+//        }
+
+//        public void printinfo()
+//        {
+//            Console.WriteLine("Student Record: ");
+//            Console.WriteLine("\tName       : " + name);
+//            Console.WriteLine("\tRollNo     : " + rollno);
+//            Console.WriteLine("\tAge        : " + age);
+//        }
+//    }
+
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            student[] S = new student[2];
+            
+//            S[0] = new student();
+//            S[1] = new student();
+
+//            S[0].Setinfo("Herry", 101, 25);
+//            S[0].printinfo();
+
+//            S[1].Setinfo("Potter", 102, 27);
+//            S[1].printinfo();
+//        }
+//    }
+//}
