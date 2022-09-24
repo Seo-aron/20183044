79a5971d5ec74289cc0b008acbb390c21718a4e0
Seo-aron <113684586+Seo-aron@users.noreply.github.com>
2022-09-24 21:21:02 +0900

    Add files via upload

---
+++ area.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEST
+{
+    internal class area
+    {
+        static void Main(String[] args)
+        {
+            double height, wid;
+            double area;
+            string line;
+            string[] field;
+
+            Console.Write("Enter height & width  :  ");
+            line = Console.ReadLine();
+            field = line.Split(' ');
+
+            height = double.Parse(field[0]);
+            wid = double.Parse(field[1]);
+            area = height * wid;
+            Console.WriteLine("area = " + area);
+        }
+    }
+}
