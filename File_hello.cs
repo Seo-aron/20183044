81827fa99668fc60d07e96cbf76d051b2f01773a
Seo-aron <113684586+Seo-aron@users.noreply.github.com>
2022-10-04 22:29:04 +0900

    Add files via upload

---
+++ File_hello.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace test01
+{
+    class File_hello
+    {
+        static void Main()
+        {
+            FileStream fs;
+
+            try
+            {
+                fs = new FileStream("file2.txt", FileMode.Create);
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("파일을 열 수 없습니다.");
+                return;
+            }
+
+            StreamWriter w = new StreamWriter(fs);
+            w.Write("안녕하세요. C#");
+            w.Close();
+
+            Console.WriteLine("안녕하세요. C#");
+        }
+    }
+}
