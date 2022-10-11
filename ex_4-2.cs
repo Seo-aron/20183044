f17f5c7688f2fab95567c4c039fa4905a72b2387
Seo-aron <113684586+Seo-aron@users.noreply.github.com>
2022-10-11 18:26:59 +0900

    Add files via upload

---
+++ ex_4-2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class StaticVsInstanceApp
+{
+    int instanceVariable; // private : default
+    static int staticVariable;
+    public static void Main()
+    {
+        StaticVsInstanceApp obj = new StaticVsInstanceApp();
+        obj.instanceVariable = 10; // 객체
+        //StaticVsInstanceApp.instanceVariable = 10; // error
+
+        StaticVsInstanceApp.staticVariable = 20; // 클래스                                       
+        //obj.staticVariable = 20; // error
+
+        Console.WriteLine("instance variable={0}, static variable={1}",
+        obj.instanceVariable, StaticVsInstanceApp.staticVariable);
+    }
+}
