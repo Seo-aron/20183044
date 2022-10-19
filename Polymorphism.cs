729a9ab6f65415908b670b0d08c889ae511bac42
Seo-aron <113684586+Seo-aron@users.noreply.github.com>
2022-10-19 19:00:15 +0900

    Add files via upload

---
+++ Polymorphism.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Polymorphism_class_1
+{
+    class Shape
+    {
+        virtual public void Draw()
+        {
+        }
+    }
+    class Circle : Shape
+    {
+        override public void Draw()
+        {
+            Console.WriteLine("I am Circle");
+        }
+    }
+    class Rectangle : Shape
+    {
+        override public void Draw()
+        {
+            Console.WriteLine("I am Rectangle");
+        }
+    }
+    class Triangle : Shape
+    {
+        override public void Draw()
+        {
+            Console.WriteLine("I am Triangle");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Paint(new Circle());
+            Paint(new Rectangle());
+            Paint(new Triangle());
+        }
+        static void Paint(Shape shape)
+        {
+            shape.Draw();
+        }
+    }
+}
