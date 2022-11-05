e75f89ceb5b3078a5dd14aa8cfbbe55b81edc74a
Seo-aron <113684586+Seo-aron@users.noreply.github.com>
2022-11-05 15:42:16 +0900

    Add files via upload

---
+++ After Mid/Ch04/Property.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter4
+{
+    class Fraction
+    {
+        private int numerator;
+        private int denominator;
+        public int Numerator
+        {
+            get { return numerator; }
+            set { numerator = value; }
+        }
+        public int Denominator
+        {
+            get { return denominator; }
+            set { denominator = value; }
+        }
+        override public string ToString()
+        {
+            return (numerator + "/" + denominator);
+        }
+    }
+    internal class Property
+    {
+        public static void Main()
+        {
+            Fraction f = new Fraction();
+            int i;
+            f.Numerator = 1;        // Numerator에서 set을 호출
+            i = f.Numerator + 1;    // Numerator에서 get을 호출
+            f.Denominator = i;      // Denominator에서 set을 호출
+            Console.WriteLine(f.ToString());
+        }
+    }
+}
