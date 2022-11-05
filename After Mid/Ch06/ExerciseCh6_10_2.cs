2ae2ffdde270b9934292abbfe6f548a77d565eae
Seo-aron <113684586+Seo-aron@users.noreply.github.com>
2022-11-06 00:37:48 +0900

    Add files via upload

---
+++ After Mid/Ch06/ExerciseCh6_10_2.cs
@@ -0,0 +1,30 @@
+﻿//using System;
+
+//class Ex : Exception { }
+//    internal class ExerciseCh6_10_2
+//    {
+//    public static void Main()
+//    {
+//        Console.WriteLine("Entering first try block");
+//        try
+//        {
+//            Console.WriteLine("Entering second try block");
+//            try
+//            {
+//                throw new Ex();
+//            }
+//            finally
+//            {
+//                Console.WriteLine("finally in 2nd try block");
+//            }
+//        } catch(Ex e)
+//        {
+//            Console.WriteLine("Caught Ex in first try block");
+//        }
+//        finally
+//        {
+//            Console.WriteLine("finally in 1st try block");
+//        }
+//    }
+//    }
+
