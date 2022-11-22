291fee941ed8997cab775962115a4ce56f2b30c4
Seo-aron <113684586+Seo-aron@users.noreply.github.com>
2022-11-22 20:54:23 +0900

    Update ASCII.cs

--- Until Mid/ASCII.cs
+++ Until Mid/ASCII.cs
@@ -1,17 +1,17 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace _20183044
-//{
-//    internal class ASCII
-//    {
-//        static void Main(string[] args)
-//        {
-//            int value = Console.Read();
-//            Console.WriteLine("Read Result : {0}", value);
-//        }
-//    }
-//}
+namespace _20183044
+{
+    internal class ASCII
+    {
+        static void Main(string[] args)
+        {
+            int value = Console.Read();
+            Console.WriteLine("Read Result : {0}", value);
+        }
+    }
+}
