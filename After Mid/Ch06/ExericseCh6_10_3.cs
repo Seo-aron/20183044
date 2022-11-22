5800aab1405caf96cece006f8fa2da954b4c017d
Seo-aron <113684586+Seo-aron@users.noreply.github.com>
2022-11-22 20:57:39 +0900

    Update ExericseCh6_10_3.cs

--- After Mid/Ch06/ExericseCh6_10_3.cs
+++ After Mid/Ch06/ExericseCh6_10_3.cs
@@ -13,23 +13,23 @@ class FinallyClause
         }
     }
     public void MethodB()
-//    {
-//        try
-//        {
-//            Console.WriteLine("3");
-//        }
-//        finally
-//        {
-//            Console.WriteLine("4"); // finally문은 무조건 작동
-//        }
-//    }
-//}
-//class ExericseCh6_10_3
-//{
-//    public static void Main()
-//    {
-//        FinallyClause fc = new FinallyClause();
-//        fc.MethodA();
-//        fc.MethodB();
-//    }
-//}
+    {
+        try
+        {
+            Console.WriteLine("3");
+        }
+        finally
+        {
+            Console.WriteLine("4"); // finally문은 무조건 작동
+        }
+    }
+}
+class ExericseCh6_10_3
+{
+    public static void Main()
+    {
+        FinallyClause fc = new FinallyClause();
+        fc.MethodA();
+        fc.MethodB();
+    }
+}
