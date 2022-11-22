a54b316b9b5e07844472af0977adaac713e54b09
Seo-aron <113684586+Seo-aron@users.noreply.github.com>
2022-11-22 21:21:51 +0900

    Update TIME.cs

--- Until Mid/TIME.cs
+++ Until Mid/TIME.cs
@@ -11,8 +11,8 @@ namespace _20183044
         public static void Main()
         {
             int time, hour, minute, second;
-//            Console.WriteLine("***Enter an integral time: ");
-//            time = int.Parse(Console.ReadLine());
+            Console.WriteLine("***Enter an integral time: ");
+            time = int.Parse(Console.ReadLine());
 //            hour = time / 10000;
 //            minute = time / 100 % 100;
 //            second = time % 100;
