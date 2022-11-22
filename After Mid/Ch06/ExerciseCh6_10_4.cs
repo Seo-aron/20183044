8eb03c04a3592a8e1e9261f5cf14d56b3eb5203f
Seo-aron <113684586+Seo-aron@users.noreply.github.com>
2022-11-22 20:59:10 +0900

    Update ExerciseCh6_10_4.cs

--- After Mid/Ch06/ExerciseCh6_10_4.cs
+++ After Mid/Ch06/ExerciseCh6_10_4.cs
@@ -16,18 +16,18 @@ namespace Chapter6
             Console.WriteLine("DONE! " + Thread.CurrentThread.Name);
         }
     }
-//    class ExerciseCh6_10_4
-//    {
-//        public static void Main()
-//        {
-//            SimpleThread obj = new SimpleThread();
-//            ThreadStart ts = new ThreadStart(obj.SimpleMethod);
-//            Thread worker1 = new Thread(ts);
-//            Thread worker2 = new Thread(ts);
-//            worker1.Name = "Apple";
-//            worker2.Name = "Orange";
-//            worker1.Start(); // worker1 스레드 실행
-//            worker2.Start();
-//        }
-//    }
-//}
+    class ExerciseCh6_10_4
+    {
+        public static void Main()
+        {
+            SimpleThread obj = new SimpleThread();
+            ThreadStart ts = new ThreadStart(obj.SimpleMethod);
+            Thread worker1 = new Thread(ts);
+            Thread worker2 = new Thread(ts);
+            worker1.Name = "Apple";
+            worker2.Name = "Orange";
+            worker1.Start(); // worker1 스레드 실행
+            worker2.Start();
+        }
+    }
+}
